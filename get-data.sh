c761ed84244c3cc2fe84a60fdeb346df059a4ddc
Clay <cwells73@gmail.com>
2016-01-18 01:54:23 -0500

    adding shell script that calls the report script. getdays is an optional argument.

---
+++ get-data.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Get all data from past XX5 days then XX4, XX3, etc..
+# subtract day XX5 from day XX4 for that day's total.
+
+# The location of HECTOR
+HECTORSCRIPTS="/opt/hector/app/scripts"
+cd ${HECTORSCRIPTS}
+
+# Create the results directory if it does not exist
+if [ ! -d darknet-results ]
+then
+  echo "[ ] directory: darknet-results does not exist.. creating."
+  mkdir darknet-results
+fi
+
+# How many days in the past would you like to query?
+getday=365
+
+#echo "[+] getday: ${getday}"
+
+# Iterate over the past days to query
+while [ $getday -gt "1" ]
+do
+  getday=$(($getday-1))
+  # Name files so they appear sequentially (could use case)
+  if [ $getday -lt "100" ]
+  then
+    ofile="dn0${getday}.csv"
+  else
+    ofile="dn${getday}.csv"
+  fi
+  if [ $getday -lt "10" ]
+  then
+    ofile="dn00${getday}.csv"
+  fi
+
+  # Call the report script and save to $ofile
+  python report-darknet.py $getday > darknet-results/$ofile
+  #echo "[+] getday: ${getday}, ofile ${ofile}"
+done
